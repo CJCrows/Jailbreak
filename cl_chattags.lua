a766d7bb58773e940b74d87aa1527dc0ffc1d194
Devocity <chajecraft@hotmail.com>
2015-05-05 17:38:19 +0200

    chattags 4 crowsybae

---
+++ cl_chattags.lua
@@ -0,0 +1,29 @@
+CT.Tags = {
+	["owner"] = {Color(85,0,150), "[Owner]"},
+	["coowner"] = {Color(90,50,150), "[Co-Owner]"},
+	
+	["headadmin"] = {Color(180,50,50), "[Head Admin]"},
+	
+	["sadmin"] = {Color(180,50,100), "[S-Admin]"},
+	["superadmin"] = {Color(180,50,100), "[S-Admin]"},
+	
+	["admin"] = {Color(255,150,50), "[Admin]"},
+	["mod"] = {Color(100,200,255), "[Mod]"},
+	["moderator"] = {Color(100,200,255), "[Moderator]"},
+	
+	["dev"] = {Color(100,200,255), "[Dev]"},
+	["developer"] = {Color(100,200,255), "[Developer]"},
+	["coder"] = {Color(100,200,255), "[Coder]"},
+	
+	["trusted"] = {Color(50,200,180), "[Trusted]"},
+	["respected"] = {Color(50,200,180), "[Respected]"},
+	["regular"] = {Color(50,200,180), "[Regular]"},
+	
+	["trial"] = {Color(50,200,180), "[Trial]"},
+	["trialmod"] = {Color(50,200,180), "[Trial Mod]"},
+	["trialadmin"] = {Color(50,200,180), "[Trial Admin]"},
+	
+	["donator"] = {Color(100,200,50), "[Donator]"},
+	["vip"] = {Color(100,200,50), "[VIP]"},
+	["vip+"] = {Color(180,200,100), "[VIP+]"},
+}
